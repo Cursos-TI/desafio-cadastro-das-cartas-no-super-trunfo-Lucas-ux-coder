612c8c13c9479253ac21d90c261847a15b9390b5
Lucas-ux-coder <ingridpirespaulino@hotmail.com>
2025-02-24 12:32:27 +0000

    jogo super trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,7 +3,8 @@ int main(){
 char cidade[99] = "Rio de janeiro";
 char nome = 'c';
 char codigo[99] = "A02";
-long int populacao = 3333;
+long int populacao1 = 3333;
+long int populacao2 = 3333;
 float area = 444.9;
 float pib = 333.9;
 int turismo = 40;
@@ -21,7 +22,7 @@ printf("Coloque a letra do estado seguida de um número de 01 a 04 (ex: A01, B03
 scanf("%s",&codigo);
 
 printf("Insira a população da sua cidade:\n");
-scanf("%ld", &populacao);
+scanf("%ld", &populacao1);
 
 printf("Insira a área da sua cidade Km²:\n");
 scanf("%f", &area);
@@ -33,15 +34,15 @@ printf("Quantos pontos turísticos tem na sua cidade?:\n");
 scanf("%d", &turismo);
 
 // Cálculo da densidade populacional 01
-area = (float) populacao / area;
+area = (float) populacao1 / area;
 printf("\n\nA densidade poulacional é: %f\n", area);
 
 // Cálculo do PIB per capta 01
-pib = (float) populacao / pib;
+pib = (float) populacao1 / pib;
 printf("\n\nO PIB per capta da sua cidade é: %f\n", pib);
 
 // super poder da carta 01
-super1 = (float) (populacao / area) + (populacao / pib) + populacao + pib + area;
+super1 = (float) (populacao1 / area) + (populacao1 / pib) + populacao1 + pib + area;
 printf("\nO pder da sua carta é: %f\n", super1);
 
 
@@ -60,7 +61,7 @@ printf("Coloque a letra do estado seguida de um número de 01 a 04 (ex: A01, B03
 scanf("%s",&codigo);
 
 printf("Insira a população da sua cidade:\n");
-scanf("%ld", &populacao);
+scanf("%ld", &populacao2);
 
 printf("Insira a área da sua cidade Km²:\n");
 scanf("%f", &area);
@@ -72,25 +73,38 @@ printf("Quantos pontos turísticos tem na sua cidade?:\n");
 scanf("%d", &turismo);
 
 // Cálculo da densidade populacional carta 02
-area = (float) populacao / area;
+area = (float) populacao2 / area;
 printf("\n\nA densidade poulacional é: %f\n", area);
 
 // Cálculo do PIB per capta carta 02
-pib = (float) populacao / pib;
+pib = (float) populacao2 / pib;
 printf("\n\nO PIB per capta da sua cidade é: %f\n", pib);
 
 // Cálculo do super poder da carta 02
-super2 = (float) (populacao / area) + (populacao / pib) + populacao + pib + area;
+super2 = (float) (populacao2 / area) + (populacao2 / pib) + populacao2 + pib + area;
 printf("\nO pder da sua carta é: %f\n", super2);
 
 // Comparação das cartas 
-printf("\n\n\n Carta 1 venceu?:\n %d",(int) super1 > super2 );
+if (populacao1 > populacao2)
+{
+   printf("\nCarta 01 venceu!!\n");
+} else
+{
+printf("\nCarta 02 venceu!!\n");
+}
+
+
 
 
 return 0;
 
-
-
-
-
 }
+
+
+
+
+
+
+
+
+
